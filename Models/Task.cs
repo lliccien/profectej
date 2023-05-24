79e915fb3d59239ddbefaa57a920ac6938e0d7d0
Ludwring Liccien <ludwring.liccien@gmail.com>
2023-05-24 10:43:37 -0500

    basic configuration, inmemory and postgres

---
+++ Models/Task.cs
@@ -0,0 +1,31 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace projectef.Models;
+
+public class Task
+{
+    [Key]
+    public Guid TaskId {get;set;}
+
+    [ForeignKey("CategoryId")]
+    public Guid CategoryId {get;set;}
+
+    [Required]
+    [MaxLength(200)]
+    public string Title {get;set;}
+    public string Description {get;set;}
+    public Priority PriorityTask {get;set;}
+    public DateTime createdAt {get;set;}
+    public virtual Category Category {get;set;}
+
+    [NotMapped]
+    public string Resume {get;set;}
+}
+
+public enum Priority
+{
+    Low,
+    Medium,
+    High
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using projectef;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// builder.Services.AddDbContext<TasksContext>(p => p.UseInMemoryDatabase("tasksDB"));
+builder.Services.AddDbContext<TasksContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("psql"))
+);
+
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/dbconexion", async ([FromServices] TasksContext dbContext) =>
+{
+    dbContext.Database.EnsureCreated();
+    // return Results.Ok("Databse in memory runing: " + dbContext.Database.IsInMemory());
+    return Results.Ok("Database in postgreSQL runing: " + dbContext.Database.IsNpgsql());
+});
+
+app.Run();
